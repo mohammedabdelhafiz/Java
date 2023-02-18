b8e259d6d19f1f4232e464f14af8322aee371a39
Mohammed Abdelhafiz <mg.abdelhafiz@gmail.com>
2023-02-19 00:22:06 +0200

    commit

---
+++ test/src/test/apples.java
@@ -0,0 +1,8 @@
+package test;
+
+public class apples{
+	public static void main(String args[]){
+		System.out.println("Hello Momo!! <3");
+	}
+	
+}
---
+++ variables/src/variables/integers.java
@@ -0,0 +1,18 @@
+package variables;
+
+public class integers {
+	public static void main (String args[]) {
+		double tuna;
+		tuna = 50.2;
+		
+		System.out.println("This is a "+ tuna);
+		System.out.println("This is a "+ tuna);
+		System.out.println(tuna);
+		
+		
+		
+	
+		
+		
+	}
+}
