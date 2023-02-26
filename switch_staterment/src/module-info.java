e66656e4c5d429577da302fe5e88c92dfeeb2435
Mohammed Abdelhafiz <114105942+mohammedabdelhafiz@users.noreply.github.com>
2023-02-27 00:07:42 +0200

    Add files via upload

---
+++ switch_staterment/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Ghawwar
+ *
+ */
+module switch_staterment {
+}
---
+++ switch_staterment/src/switch_staterment/switchh.java
@@ -0,0 +1,30 @@
+package switch_staterment;
+
+public class switchh {
+	public static void main (System args[]) {
+		int age;
+		age=1;
+		
+		
+		switch (age) {
+		case 1:
+			System.out.println("you can crawl");
+			break;
+			
+		case 2:
+			System.out.println("you can Walk");
+			break;
+			
+		case 3:
+			System.out.println("you can RUN ! tararararrara");
+			break;
+			
+		default:
+			System.out.println("i dont undertand anything :(");
+			break;
+			
+			
+		}
+	}
+
+}
