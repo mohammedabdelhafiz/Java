a2ad89ce297ac767224957a50d506883bfd7f679
Mohammed Abdelhafiz <114105942+mohammedabdelhafiz@users.noreply.github.com>
2023-02-27 00:08:04 +0200

    Add files via upload

---
+++ whilee/src/whilee/people.java
@@ -0,0 +1,12 @@
+package whilee;
+
+public class people {
+	public static void main (System args[]) {
+		int counter=0;
+		while (counter < 10) {
+			System.out.println("counter");
+			counter++;
+		}
+		
+	}
+}
