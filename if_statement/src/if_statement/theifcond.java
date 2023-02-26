cb3310dc58b094e704edc1d68aa7614e16d86b7f
Mohammed Abdelhafiz <114105942+mohammedabdelhafiz@users.noreply.github.com>
2023-02-26 21:08:43 +0200

    Add files via upload

---
+++ if_statement/src/if_statement/theifcond.java
@@ -0,0 +1,20 @@
+package if_statement;
+
+public class theifcond {
+	public static void main (String args[]) {
+		int test = 6;
+		
+		if ( test ==9 ) {
+			System.out.println("YESSS");
+		}
+		
+		else if(test!=10) {
+			System.out.println(" NO ");
+		}
+		
+		else {
+			System.out.println("Maybe");
+		}
+		
+	}
+}
---
+++ if_statement/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Ghawwar
+ *
+ */
+module if_statement {
+}
