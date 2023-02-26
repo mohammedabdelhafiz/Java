d486d420f1acb233877b9dd6e8f380550e2ee907
Mohammed Abdelhafiz <114105942+mohammedabdelhafiz@users.noreply.github.com>
2023-02-27 00:07:28 +0200

    Add files via upload

---
+++ multi_classes/src/multi_classes/apples.java
@@ -0,0 +1,10 @@
+package multi_classes;
+
+public class apples {
+	public static void main (System args[]) {
+		
+		tuna tunaObject = new tuna() ;
+		tunaObject.simpleMessage();
+	}
+
+}
---
+++ multi_classes/src/multi_classes/tuna.java
@@ -0,0 +1,7 @@
+package multi_classes;
+
+public class tuna {
+	public void simpleMessage() {
+		System.out.println("this is tuna class");
+	}
+}
