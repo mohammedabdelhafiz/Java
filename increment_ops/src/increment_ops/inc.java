f0d75ae6b669e3d5fc8076322fc27547e376cbcf
Mohammed Abdelhafiz <114105942+mohammedabdelhafiz@users.noreply.github.com>
2023-02-26 20:06:59 +0200

    Add files via upload

---
+++ increment_ops/src/increment_ops/inc.java
@@ -0,0 +1,17 @@
+package increment_ops;
+import java.util.Scanner;
+public class inc {
+	public static void main(String args[]) {
+		Scanner bucky=new Scanner (System.in);
+		int momo = 14;
+		
+		int	cat =5;
+		cat +=5;
+		
+		
+	
+		System.out.println(cat);
+
+	}
+
+}
